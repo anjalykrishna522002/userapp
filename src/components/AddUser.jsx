82cd018222d5e7cda2e5dcfa0ac49d8e4f960b82
anjalykrishna522002 <anjalykrishnaas@gmail.com>
2024-06-08 11:05:17 +0530

    done

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -14,7 +14,15 @@ const AddUser = () => {
                         <input type="text" className="form-control" />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <label htmlFor="" className="form-label">User Name</label>
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">UserName</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Email</label>
                         <input type="text" className="form-control" />
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -22,11 +30,11 @@ const AddUser = () => {
                         <textarea name="" id="" className="form-control"></textarea>
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <label htmlFor="" className="form-lable">pincode</label>
+                        <label htmlFor="" className="form-label">phone</label>
                         <input type="text" className="form-control" />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <label htmlFor="" className="form-label">Email Id</label>
+                        <label htmlFor="" className="form-label">website</label>
                         <input type="text" className="form-control" />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
